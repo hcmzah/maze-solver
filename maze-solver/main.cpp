141f16b9e178c23d207a55cddc5eefe397bff598
Hamzah <42479150+hcmzah@users.noreply.github.com>
2024-08-14 00:37:30 +0100

    change to imgui docking for transparency

--- maze-solver/main.cpp
+++ maze-solver/main.cpp
@@ -3,59 +3,85 @@
 #include <imgui.h>
 #include <imgui_impl_opengl3.h>
 #include <GLFW/glfw3.h>
-
 #include <imgui_impl_glfw.h>
 
 static GLFWwindow* window = nullptr;
 
 int main() {
 
-	if (!glfwInit()) {
-		std::cout << "Failed to initialise GLFW" << std::endl;
-		std::cin.get();
-	}
+    if (!glfwInit()) {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        std::cin.get();
+        return -1;
+    }
 
-	window = glfwCreateWindow(1280, 720, "Maze Solver", nullptr, nullptr);
+    glfwWindowHint(GLFW_DECORATED, GLFW_FALSE); // No window borders or controls
+    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE); // Transparent framebuffer
 
-	glfwMakeContextCurrent(window);
+    window = glfwCreateWindow(1, 1, "Maze Solver", nullptr, nullptr);
+    if (!window) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
 
-	ImGui::CreateContext();
+    glfwMakeContextCurrent(window);
 
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init();
+    ImGui::CreateContext();
 
-	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable; // Enable docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable; // Enable multi-viewport
+    io.IniFilename = NULL;
+    io.LogFilename = NULL;
 
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init();
 
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.6f, 0.f);
 
-		ImGui::Begin("Maze Solver");
-		ImGui::End();
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
 
-		ImGui::Render();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
 
-		int display_w, display_h;
+        ImGui::SetNextWindowSize(ImVec2(800, 600), ImGuiCond_FirstUseEver);
+        ImGui::Begin("Maze Solver", nullptr,  ImGuiWindowFlags_NoCollapse);
 
-		glfwGetFramebufferSize(window, &display_w, &display_h);
-		glViewport(0, 0, display_w, display_h);
-		glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
-		glClear(GL_COLOR_BUFFER_BIT);
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        ImGui::End();
 
-		glfwSwapBuffers(window);
-	}
+        ImGui::Render();
 
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-	if (window) {
-		glfwDestroyWindow(window);
-	}
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-	glfwTerminate();
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+            GLFWwindow* backup_current_context = glfwGetCurrentContext();
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+            glfwMakeContextCurrent(backup_current_context);
+        }
+
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    if (window) {
+        glfwDestroyWindow(window);
+    }
+    glfwTerminate();
+
+    return 0;
 }
